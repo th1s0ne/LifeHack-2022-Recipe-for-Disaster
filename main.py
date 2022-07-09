3fd6ace243bec642661e21977c7fee47e10eae84
Yongjian <85063193+Krueless@users.noreply.github.com>
2022-07-09 16:28:34 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ from telegram.ext import (
 )
 
 
+async def start(update, context):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="",
+    )
+
 
 async def unknown(update, context):
     await context.bot.send_message(
@@ -21,15 +27,35 @@ async def unknown(update, context):
     )
 
 
+async def search(update, context):
+    pass
+
+
+async def learn(update, context):
+    pass
+
+
+async def donate(update, context):
+    pass
+
+
 if __name__ == "__main__":
     load_dotenv()
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     API_Token = os.getenv("API_TOKEN")
     API_ID = os.getenv("API_ID")
     application = ApplicationBuilder().token(BOT_TOKEN).build()
+
     e = Edamam(recipes_appid=API_ID, recipes_appkey=API_Token)
-    
+
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    search_handler = CommandHandler("search", search)
+    learn_handler = CommandHandler("learn", learn)
+    donate_handler = CommandHandler("donate", donate)
+
     application.add_handler(unknown_handler)
-    
+    application.add_handler(search_handler)
+    application.add_handler(learn_handler)
+    application.add_handler(donate_handler)
+
     application.run_polling()
