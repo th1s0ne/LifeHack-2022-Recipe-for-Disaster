763631c46820e043c84ef9d3149900ded71ba1fe
th1s0ne <85068821+th1s0ne@users.noreply.github.com>
2022-07-10 09:53:14 +0800

    /donate function
    remember to insert the relevant new handlers

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
-from telegram import Update
-
+from telegram import CallbackQuery, Update
+from DonationLocation import donate, areaRetriever
 from py_edamam import Edamam
 
 
@@ -10,15 +10,29 @@ from telegram.ext import (
     MessageHandler,
     ApplicationBuilder,
     CommandHandler,
+    CallbackQueryHandler
 )
 
 
 async def start(update, context):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="",
+        text="Testing",
     )
 
+async def location(update, context):
+    user = update.effective_user
+    message = None
+
+    global prev_message
+
+    if update.edited_message:
+        message = update.edited_message
+    else:
+        message = update.message
+
+    current_pos = (message.location.latitude, message.location.longitude)
+    print(current_pos)
 
 async def unknown(update, context):
     await context.bot.send_message(
@@ -35,10 +49,6 @@ async def learn(update, context):
     pass
 
 
-async def donate(update, context):
-    pass
-
-
 if __name__ == "__main__":
     load_dotenv()
     BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -48,14 +58,15 @@ if __name__ == "__main__":
 
     e = Edamam(recipes_appid=API_ID, recipes_appkey=API_Token)
 
-    unknown_handler = MessageHandler(filters.COMMAND, unknown)
     search_handler = CommandHandler("search", search)
     learn_handler = CommandHandler("learn", learn)
     donate_handler = CommandHandler("donate", donate)
-
-    application.add_handler(unknown_handler)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    
     application.add_handler(search_handler)
     application.add_handler(learn_handler)
     application.add_handler(donate_handler)
+    application.add_handler(CallbackQueryHandler(areaRetriever))
+    application.add_handler(unknown_handler)
 
     application.run_polling()
